521a2cb2dbbc617de4efd403c97513ab9029a737
Anirudh Jwala <anirudhsai2@gmail.com>
2025-02-07 00:17:18 +0530

    feat: add ProductList component and sample data for displaying products on Home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,16 @@
+import ProductList from '@/components/shared/product/product-list';
+import sampleData from '@/db/sample-data';
+
 const Home = () => {
-  return <>Crochet Boutique</>;
+  return (
+    <>
+      <ProductList
+        data={sampleData.products}
+        title='Newest Arrivals'
+        limit={4}
+      />
+    </>
+  );
 };
 
 export default Home;
