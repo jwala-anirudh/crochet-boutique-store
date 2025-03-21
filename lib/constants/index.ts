86320484ba2a225e4b75076f7ff2bd8f2ae4f4b3
Anirudh Jwala <anirudhsai2@gmail.com>
2025-03-22 03:40:46 +0530

    feat: create layout and homepage components; update metadata and add constants

---
+++ lib/constants/index.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Crochet Boutique";
+export const APP_DESCRIPTION =
+  process.env.NEXT_PULIC_APP_DESCRIPTION ||
+  "Your one-stop shop for handmade crochet items";
+export const SERVER_URL =
+  process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
