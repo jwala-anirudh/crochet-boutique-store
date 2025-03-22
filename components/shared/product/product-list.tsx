e5fe525f4690ef4780098190a94e008ccb0285a4
Anirudh Jwala <anirudhsai2@gmail.com>
2025-03-22 13:50:25 +0530

    feat: add ProductCard and Card components; update ProductList to use ProductCard for rendering products

--- components/shared/product/product-list.tsx
+++ components/shared/product/product-list.tsx
@@ -1,4 +1,5 @@
 import { Product } from "@/db/sample-data";
+import ProductCard from "./product-card";
 
 const ProductList = ({
   data,
@@ -17,7 +18,7 @@ const ProductList = ({
       {data.length > 0 ? (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
           {limitedData.map((product: Product) => (
-            <div key={product.name}>{product.name}</div>
+            <ProductCard key={product.slug} product={product} />
           ))}
         </div>
       ) : (
